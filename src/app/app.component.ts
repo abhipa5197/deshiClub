5c9fbbce7f9081c88ed3a2d2f50e6e10f5c3d880
abhishek pandey <pandeyabhishek@siemens.com>
2025-09-06 16:45:55 +0530

    Created header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { StatsComponent } from './stats/stats.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [HeaderComponent, HeroComponent, StatsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title(title: any) {
-    throw new Error('Method not implemented.');
+ 
   }
-
-}
