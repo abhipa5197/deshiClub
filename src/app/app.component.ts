671ef9662b15e045b17cf954ba383bfefa1c9cd4
abhishek pandey <pandeyabhishek@siemens.com>
2025-09-09 01:12:47 +0530

    added landing image for work under progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
 import { StatsComponent } from './stats/stats.component';
+import { Router, RouterOutlet } from '@angular/router';
+import { TempViewComponent } from './temp-view/temp-view.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, HeroComponent, StatsComponent],
+  imports: [HeaderComponent, HeroComponent, StatsComponent, RouterOutlet, TempViewComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
- 
+ constructor(public router: Router) {}
   }
